41e0158b6d96cc2b75bad89a47110ffbe330dd2c
kanywst <niwatakuma@icloud.com>
2026-01-06 16:27:41 +0900

    fix: update 'no-unused-vars' rule to warn for unused variables with specific pattern

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'off',
+    'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
     'no-undef': 'off' // Wails bindings use global window.go etc
   }
 }
